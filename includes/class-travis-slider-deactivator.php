4e17a296fdca766ed2eb6f41725ca339ea5333b5
rezak1618 <re.kabiri@gmail.com>
2019-12-31 00:56:54 +0400

    Initial commit

---
+++ includes/class-travis-slider-deactivator.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://b1stack.com/
+ * @since      1.0.0
+ *
+ * @package    Travis_Slider
+ * @subpackage Travis_Slider/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Travis_Slider
+ * @subpackage Travis_Slider/includes
+ * @author     Reza Kabiri <re.kabiri@gmail.com>
+ */
+class Travis_Slider_Deactivator {
+	/**
+	 * When disabled Travis Slider PLugin
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+	}
+}
